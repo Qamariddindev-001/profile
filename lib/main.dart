9e7451834596c5f845ad91c088f3168f7494239b
User <qamariddinol664@gmail.com>
2023-06-03 10:00:21 +0500

    phone

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,52 @@
 import 'dart:convert';
-import 'package:profile/http.dart' as http;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http1;
+import 'package:http/http.dart' as http;
 
-Future<String> getPhoneNumber() async {
+Future<String> getfullName() async {
   print('started');
+
   String url = 'https://randomuser.me/api/';
   Uri uri = Uri.parse(url);
 
-  http1.Response response = await http.get(uri);
+  http.Response response = await http.get(uri);
 
   Map m = jsonDecode(response.body);
+
   print(m);
+  return m['results'][0]['name']['first'] +
+      '' +
+      m['results'][0]['name']['last'];
+}
+
+Future<String> getphoneNumber() async {
+  print('started');
+
+  String url = 'https://randomuser.me/api/';
+  Uri uri = Uri.parse(url);
+
+  http.Response response = await http.get(uri);
+
+  Map m = jsonDecode(response.body);
+
+  print('');
   return m['results'][0]['phone'];
+}
+
+Future<String> getlarge() async {
+  print('started');
+
+  String url = 'https://randomuser.me/api/';
+  Uri uri = Uri.parse(url);
+
+  http.Response response = await http.get(uri);
+
+  Map m = jsonDecode(response.body);
+
+  print('large');
+
+  return m["results"][0]['picture']['large'];
 }
 
 void main() {
@@ -29,83 +62,93 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String phoneNumber = '1234';
+  String fullName = 'Oltiboyev Qamar';
+  String large = 'image';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 21.0, top: 11),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 211, 226, 248),
-                          borderRadius: BorderRadius.circular(50),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 21.0, top: 11),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 211, 226, 248),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.arrow_back,
+                                color: Color.fromARGB(255, 62, 127, 224),
+                              )),
                         ),
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.arrow_back,
-                              color: Color.fromARGB(255, 62, 127, 224),
-                            )),
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 17),
+                  const Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.99,
                     ),
-                  ],
-                ),
-                const SizedBox(height: 17),
-                const Text(
-                  'Profile',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 0.99,
                   ),
-                ),
-                const SizedBox(height: 43),
-                CircleAvatar(
-                  radius: 80.5,
-                  child: Image.asset('assets/Ellipse 4.png'),
-                ),
-                const SizedBox(height: 51),
-                const Rectangle(name: 'Somto Ferdinand', icon: Icons.person),
-                const SizedBox(height: 21),
-                Rectangle(name: phoneNumber, icon: Icons.phone),
-                const SizedBox(height: 21),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
-                    minimumSize: MaterialStateProperty.all(const Size(325, 50)),
+                  const SizedBox(height: 43),
+                  CircleAvatar(
+                    radius: 80.5,
+                    backgroundImage: NetworkImage(large),
                   ),
-                  onPressed: () async {
-                    phoneNumber = await getPhoneNumber();
-                    setState(() {});
-                  },
-                  child: const Text(
-                    'Next',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  const SizedBox(height: 51),
+                  Rectangle(name: fullName, icon: Icons.person),
+                  const SizedBox(height: 21),
+                  Rectangle(name: phoneNumber, icon: Icons.phone),
+                  const SizedBox(height: 21),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                      minimumSize:
+                          MaterialStateProperty.all(const Size(325, 50)),
+                    ),
+                    onPressed: () async {
+                      fullName = await getfullName();
+                      phoneNumber = await getphoneNumber();
+                      large = await getlarge();
+                      setState(() {});
+                    },
+                    child: const Text(
+                      'Next',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.medical_services_outlined, size: 40, color: Color.fromARGB(255, 62, 127, 224)),
-                Text(
-                  'TALKBOX',
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                ),
-              ],
-            )
-          ],
+                ],
+              ),
+              const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.medical_services_outlined,
+                      size: 40, color: Color.fromARGB(255, 62, 127, 224)),
+                  Text(
+                    'TALKBOX',
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -127,7 +170,9 @@ class Rectangle extends StatelessWidget {
       width: 325,
       height: 50,
       padding: const EdgeInsets.only(left: 19, right: 65, top: 16, bottom: 16),
-      decoration: BoxDecoration(color: const Color.fromARGB(255, 228, 227, 227), borderRadius: BorderRadius.circular(30)),
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 228, 227, 227),
+          borderRadius: BorderRadius.circular(30)),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
